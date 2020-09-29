fed5353906d839ba96d0b7589e5024cfa0d991bc
Zenzi Ali <zeeathome935@gmail.com>
2020-09-29 16:58:06 -0400

    question one answered

--- drills.js
+++ drills.js
@@ -1,1 +1,11 @@
-const BinarySearchTree = requires('./BinarySearchTree.js');
+const BinarySearchTree = require('./BinarySearchTree.js');
+
+
+BinarySearchTree.MAX_LOAD_RATIO = 0.5;
+BinarySearchTree.SIZE_RATIO = 3;
+// CREATE A BINARY SEARCH TREE CLASS //
+function main() {
+  const watev = new BinarySearchTree();
+}
+
+main();
